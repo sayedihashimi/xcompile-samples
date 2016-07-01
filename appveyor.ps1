d1abb759d62be193cc4da15a8f032a7b3e006c5d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-06-30 21:32:36 -0700

    updating to create different report files

--- appveyor.ps1
+++ appveyor.ps1
@@ -21,17 +21,40 @@ if(-not (Test-Path $helperpath -PathType Leaf)){
 # inline the script
 . $helperpath
 
-[string[]]$repoDirs = @()
-
+#[string[]]$repoDirs = @()
+$tempDir = (Join-Path ([System.IO.Path]::GetTempPath()) ('xcompile\{0}' -f ([datetime]::Now.Ticks)))
+New-Item -Path $tempDir -ItemType Directory
 foreach($r in $repos){
-    $repoDirs += (CloneRepo -url $r)
+    $repoDir = (CloneRepo -url $r)
+    [System.IO.DirectoryInfo]$dirInfo = $r
+    
+    $reportPath = (Join-Path $tempDir ('{0}.txt' -f $dirInfo.Name))
+    Get-Ifdef -path $repoDir | Out-File $reportPath
+    Push-AppveyorArtifact $reportPath
 }
+# create zip file
+$zipfile = (Join-Path $tempDir all.zip)
+if(Test-Path -Path $zipfile){
+    Remove-Item $zipfile
+}
+
+New-ZipFile -ZipFilePath $zipfile -rootFolder $tempDir -InputObject ((Get-ChildItem -Path $tempDir -Recurse -Filter).FullName)
+Push-AppveyorArtifact $zipfile
+
+
+<#
 
 $reportPath = (Join-Path ([System.IO.Path]::GetTempPath()) 'xcompile-report.txt')
 if(Test-Path $reportPath){
     Remove-Item $reportPath
 }
 
+foreach($rd in $repoDirs){
+    $reportPath = (Join-Path
+    Get-Ifdef -path $rd
+}
+
 Get-Ifdef -path $repoDirs | Out-File $reportPath -Append
 
 Push-AppveyorArtifact $reportPath
+#>
--- helper.ps1
+++ helper.ps1
@@ -159,6 +159,71 @@ function CloneRepo{
     }
 }
 
+# Heavily modified from: http://ss64.com/ps/zip.html / http://ss64.com/ps/zip.txt
+Add-Type -As System.IO.Compression.FileSystem
+function New-ZipFile {
+	#.Synopsis
+	#  Create a new zip file, optionally appending to an existing zip...
+	[CmdletBinding()]
+	param(
+		# The path of the zip to create
+		[Parameter(Position=0, Mandatory=$true)]
+		$ZipFilePath,
+ 
+		# Items that we want to add to the ZipFile
+		[Parameter(Position=1, Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+		[Alias("PSPath","Item")]
+		[string[]]$InputObject = $Pwd,
+ 
+        [string]$rootFolder = $pwd,
+
+        [string]$relpathinzip,
+
+		# Append to an existing zip file, instead of overwriting it
+		[Switch]$Append,
+ 
+		# The compression level (defaults to Optimal):
+		#   Optimal - The compression operation should be optimally compressed, even if the operation takes a longer time to complete.
+		#   Fastest - The compression operation should complete as quickly as possible, even if the resulting file is not optimally compressed.
+		#   NoCompression - No compression should be performed on the file.
+		[System.IO.Compression.CompressionLevel]$Compression = "Optimal"
+	)
+	begin {
+		# Make sure the folder already exists
+		[string]$File = Split-Path $ZipFilePath -Leaf
+		[string]$Folder = $(if($Folder = Split-Path $ZipFilePath) { Resolve-Path $Folder } else { $Pwd })
+		$ZipFilePath = Join-Path $Folder $File
+		# If they don't want to append, make sure the zip file doesn't already exist.
+		if(!$Append) {
+			if(Test-Path $ZipFilePath) { Remove-Item $ZipFilePath }
+		}
+		$Archive = [System.IO.Compression.ZipFile]::Open( $ZipFilePath, "Update" )
+	}
+	process {
+		foreach($path in $InputObject) {
+			foreach($item in Resolve-Path $path) {
+				# Push-Location so we can use Resolve-Path -Relative
+				# This will get the file, or all the files in the folder (recursively)
+				foreach($file in Get-ChildItem $item -Recurse -File -Force | % FullName) {
+					# Calculate the relative file path
+                    $relative = InternalGet-RelativePath -fromPath $rootFolder -toPath $file
+                    if(-not [string]::IsNullOrWhiteSpace($relpathinzip)){
+                        $relative = $relpathinzip.TrimEnd('\').TrimEnd('/') + '\' + $relative.TrimEnd('\').TrimEnd('/') + '\'
+                    }
+
+					# Add the file to the zip
+					$null = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($Archive, $file, $relative, $Compression)
+				}
+			}
+		}
+	}
+	end {
+		$Archive.Dispose()
+		Get-Item $ZipFilePath
+	}
+}
+
+
 <#
 [string[]]$paths = 'C:\Data\mycode\xunit',
                     'C:\data\mycode\Newtonsoft.Json',
